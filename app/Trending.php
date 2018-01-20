71037b2a5cfe9f95be03244d48bc225b98a4a558
hazem ali <hazemali@apples-MacBook-Air.local>
2018-01-20 20:21:59 +0200

    WIP

---
+++ app/Trending.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hazemali
+ * Date: 1/20/18
+ * Time: 2:57 AM
+ */
---
+++ tests/Feature/AddAvatarTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hazemali
+ * Date: 1/17/18
+ * Time: 9:12 PM
+ */
